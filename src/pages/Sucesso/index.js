9b58947ae0c250c760f144843c1a3249805a09d5
nfnkillzone <leandrohenriquesouza@hotmail.com>
2023-07-25 10:20:23 -0300

    realizada alteraçoes

--- src/pages/Sucesso/index.js
+++ src/pages/Sucesso/index.js
@@ -1,5 +1,5 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { Header } from "../../components/header";
 export default function Sucesso() {
-    return (_jsxs("div", { children: [_jsx(Header, {}), _jsx("div", { className: " w-100% h-[800px] bg-green-500 flex justify-center align-middle", children: _jsx("h1", { className: "mt-[300px] text-9xl", children: "Compra Efetuada !!" }) })] }));
+    return (_jsxs("div", { children: [_jsx(Header, {}), _jsx("div", { className: "w-full min-h-screen bg-green-500 flex items-center justify-center", children: _jsx("h1", { className: "text-4xl md:text-9xl text-white", children: "Compra Efetuada!!" }) })] }));
 }
